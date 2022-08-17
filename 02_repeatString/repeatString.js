2abb678ef2d62d219b36acb9555cdf42a386eb11
Namin <namin22@gmail.com>
2022-08-17 16:27:56 -0300

    Ex 2: Passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,29 @@
-const repeatString = function() {
+const number = Math.floor(Math.random() * 1000)
+
+const repeatString = function(string, num) {
+
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    if (string == '') {
+        return '';
+    }
+
+    let phrase = `${string}`.repeat(num);
+    return phrase;
 
 };
 
+
+
+repeatString('hey', number)
+
 // Do not edit below this line
 module.exports = repeatString;
+
+
+// Create a variable to hold the string you're going to return, create a loop that 
+// repeats the given number of times and add the given string to the result on each loop
+
+
