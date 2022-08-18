392a8afb8005020e377a3053471bd6473abef655
Namin <namin22@gmail.com>
2022-08-18 17:54:22 -0300

    Passed test #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
 
+const removeFromArray = function() {
+    let a;
+    [a, ...rest] = [[1, 2, 3, 4], 7, 2];
+    a.splice(1, 1);
+    return a;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//expect(removeFromArray([1, 2, 3, 4], 7, 2)).toEqual([1, 3, 4]);
